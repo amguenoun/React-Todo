11faba801320d67ea35e0bc83cadc8a67e6110bb
Ahmad Guenoun <amguenoun@gmail.com>
2019-09-02 16:22:04 -0400

    set up import for todolist and passed in tasks array as props

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
 import React from 'react';
 
+import TodoList from "./components/TodoComponents/TodoList";
+import TodoForm from "./components/TodoComponents/TodoForm";
+
 class App extends React.Component {
   constructor() {
     super();
     this.state = {
-      task: []
+      tasks: [{
+        task: 'Organize Garage',
+        id: 1528817077286,
+        completed: false
+      }]
     }
   }
   // you will need a place to store your state in this component.
@@ -14,6 +21,7 @@ class App extends React.Component {
     return (
       <div>
         <h2>Welcome to your Todo App!</h2>
+        <TodoList tasks={this.state.tasks} />
       </div>
     );
   }
