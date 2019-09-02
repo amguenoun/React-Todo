a3d09c5206f1aa445088aa5ad325ee48a4004a04
Ahmad Guenoun <amguenoun@gmail.com>
2019-09-02 16:33:44 -0400

    modified task object key from task to name and created addTask function to pass to TodoForm

--- src/App.js
+++ src/App.js
@@ -4,24 +4,30 @@ import TodoList from "./components/TodoComponents/TodoList";
 import TodoForm from "./components/TodoComponents/TodoForm";
 
 class App extends React.Component {
+  // you will need a place to store your state in this component.
+  // design `App` to be the parent component of your application.
+  // this component is going to take care of state, and any change handlers you need to work with your state
   constructor() {
     super();
     this.state = {
       tasks: [{
-        task: 'Organize Garage',
+        name: 'Organize Garage',
         id: 1528817077286,
         completed: false
       }]
     }
   }
-  // you will need a place to store your state in this component.
-  // design `App` to be the parent component of your application.
-  // this component is going to take care of state, and any change handlers you need to work with your state
+
+  addTask = (task) => {
+    this.setState({ ...this.state.tasks, task });
+  }
+
   render() {
     return (
       <div>
         <h2>Welcome to your Todo App!</h2>
         <TodoList tasks={this.state.tasks} />
+        <TodoForm addItem={this.addTask} />
       </div>
     );
   }
