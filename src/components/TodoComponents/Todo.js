c1ec065a22bcbbfc96311327b4882e70a059df9d
Ahmad Guenoun <amguenoun@gmail.com>
2019-09-02 17:16:28 -0400

    added css styles to show line-through completed tasks

--- src/components/TodoComponents/Todo.js
+++ src/components/TodoComponents/Todo.js
@@ -3,7 +3,7 @@ import React from 'react'
 const Todo = (props) => {
     console.log(props.task)
     return (
-        <div onClick={() => props.toggleComplete(props.task.id)}>
+        <div onClick={() => props.toggleComplete(props.task.id)} className={props.task.completed ? 'completed' : null}>
             <p>{props.task.name}</p>
         </div>
     )
